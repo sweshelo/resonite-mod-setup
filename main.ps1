582955155011205bca4e6ed05796b5c0a284fc6c
sweshelo <sweshelo@gmail.com>
2023-10-12 13:16:06 +0900

    feat: Create Bat file to launch Resonite with args.

--- main.ps1
+++ main.ps1
@@ -23,3 +23,5 @@ if (!(Test-Path $ResoniteModsPath)){
 iwr "https://github.com/resonite-modding-group/ResoniteModLoader/releases/latest/download/ResoniteModLoader.dll" -OutFile "${ResoniteLocation}/Libraries/ResoniteModLoader.dll"
 iwr "https://github.com/resonite-modding-group/ResoniteModLoader/releases/latest/download/0Harmony.dll" -OutFile "${ResoniteLibsPath}/0Harmony.dll"
 iwr "https://github.com/badhaloninja/ResoniteModSettings/releases/latest/download/ResoniteModSettings.dll" -OutFile "${ResoniteModsPath}/ResoniteModSettings.dll"
+
+echo "`"$(${ResoniteLocation} | Split-Path | Split-Path | Split-Path)\steam.exe`" -applaunch 2519830 -LoadAssembly Libraries\ResoniteModLoader.dll" |  Out-File -FilePath Resonite.bat -Encoding utf8
