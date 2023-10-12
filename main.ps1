d39e5d24523dd8b54a86fe96e3de78998e4db03e
sweshelo <sweshelo@gmail.com>
2023-10-12 13:22:12 +0900

    fix: 引数付きで動かなかったので修正

--- main.ps1
+++ main.ps1
@@ -20,8 +20,8 @@ if (!(Test-Path $ResoniteModsPath)){
   mkdir $ResoniteModsPath
 }
 
-iwr "https://github.com/resonite-modding-group/ResoniteModLoader/releases/latest/download/ResoniteModLoader.dll" -OutFile "${ResoniteLocation}/Libraries/ResoniteModLoader.dll"
-iwr "https://github.com/resonite-modding-group/ResoniteModLoader/releases/latest/download/0Harmony.dll" -OutFile "${ResoniteLibsPath}/0Harmony.dll"
-iwr "https://github.com/badhaloninja/ResoniteModSettings/releases/latest/download/ResoniteModSettings.dll" -OutFile "${ResoniteModsPath}/ResoniteModSettings.dll"
+#iwr "https://github.com/resonite-modding-group/ResoniteModLoader/releases/latest/download/ResoniteModLoader.dll" -OutFile "${ResoniteLocation}/Libraries/ResoniteModLoader.dll"
+#iwr "https://github.com/resonite-modding-group/ResoniteModLoader/releases/latest/download/0Harmony.dll" -OutFile "${ResoniteLibsPath}/0Harmony.dll"
+#iwr "https://github.com/badhaloninja/ResoniteModSettings/releases/latest/download/ResoniteModSettings.dll" -OutFile "${ResoniteModsPath}/ResoniteModSettings.dll"
 
-echo "`"$(${ResoniteLocation} | Split-Path | Split-Path | Split-Path)\steam.exe`" -applaunch 2519830 -LoadAssembly Libraries\ResoniteModLoader.dll" |  Out-File -FilePath Resonite.bat -Encoding utf8
+echo "`"$(${ResoniteLocation} | Split-Path | Split-Path | Split-Path)\steam.exe`" -applaunch 2519830 --LoadAssembly `"${ResoniteLocation}\Libraries\ResoniteModLoader.dll`"" |  Out-File -FilePath Resonite.bat -Encoding utf8
